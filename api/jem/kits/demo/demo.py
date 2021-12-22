8f030dec643454fddaf47cde2ee5c807fb69b3c3
jthompson-ceres <jthompson@ceresimaging.net>
2021-12-22 12:06:10 -0800

    Move toggle led code to demo.py, remove unused from main.py

--- api/jem/kits/demo/demo.py
+++ api/jem/kits/demo/demo.py
@@ -2,7 +2,17 @@
 # run some example code to demonstrate JEM features
 from jem import Jem
 from neopixel import Neopixel
+import pycom
 class Demo:
     def __init__(self):
         self.jem = Jem()
         self.neopixel = Neopixel()
+
+    def toggle_led(color1=0x440000, color2=0x004400, duration=10.0):
+        start = time.time()
+        pycom.heartbeat(False)
+        while (time.time() - start) < duration:
+            pycom.rgbled(color1)
+            time.sleep(1)
+            pycom.rgbled(color2)
+            time.sleep(1)
--- api/jem/main.py
+++ api/jem/main.py
@@ -1,3 +1,8 @@
 #main.py
-from kits import kit_main
-kit_main.load_kit()
+#from kits import kit_main
+#kit_main.load_kit()]
+from jemwifi import *
+
+wifi = setup_wifi()
+from kits.demo.neopixel import Neopixel
+pix = Neopixel()
