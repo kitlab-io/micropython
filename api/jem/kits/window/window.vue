574e8cdedf84333ff83737da32fd7ad3cff001aa
JoshuaBThompson <jbthompson.eng@gmail.com>
2022-04-10 19:53:32 -0700

    comment out removeChilds

--- api/jem/kits/window/window.vue
+++ api/jem/kits/window/window.vue
@@ -383,7 +383,7 @@ module.exports = {
       const qtdDivs = Array(length).fill(0)
 
       this.code = CODE_START
-      this.removeChilds()
+      //this.removeChilds()
       /*
       0  1  2  3
       7  6  5  4
