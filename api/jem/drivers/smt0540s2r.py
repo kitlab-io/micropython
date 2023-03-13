f51d7fc8e0c62910ebe924de4c4328ff896e30f2
jthompson-ceres <jthompson@ceresimaging.net>
2023-03-12 18:05:00 -0700

    Update buzzer driver for jem2

--- api/jem/drivers/smt0540s2r.py
+++ api/jem/drivers/smt0540s2r.py
@@ -1,20 +1,26 @@
 # smt0540s2r.py
 # buzzer code for PN: SMT-0540-S-2-R
-from machine import PWM
+from machine import PWM, Pin
 
+"""
+ex:
+>> from drivers.smt0540s2r import SMT0540S2R
+>> buzz = SMT0540S2R()
+>> buzz.start(freq_hz=100)
+>> buzz.stop()
+>> buff.pwm.duty(100) # 0 - 1024, so 100 = 100/1024 = 10% (about)
+"""
 class SMT0540S2R:
-    DUTY_CYCLE = 0.5 # don't change this!
-    def __init__(self, sig_pin="P7", channel = 0, freq_hz = 400):
+    DUTY_CYCLE = 512 # don't change this!, 0 - 1024, 512 = 50% duty
+    # default esp32 jem-2 IO32
+    def __init__(self, sig_pin=32, freq_hz = 400):
         self.sig_pin = sig_pin
         self.freq_hz = freq_hz
-        self.channel = channel
-        self.pwm = PWM(self.channel, self.freq_hz)
-        self.pwm_sig = self.pwm.channel(self.channel, pin=self.sig_pin, duty_cycle=0)
+        self.pwm = PWM(Pin(sig_pin), freq=500, duty=0)
 
     def start(self, freq_hz=100):
-        self.pwm = PWM(self.channel, freq_hz)
-        self.pwm_sig.duty_cycle(SMT0540S2R.DUTY_CYCLE)
-        self.freq_hz = freq_hz
+        self.pwm.duty(SMT0540S2R.DUTY_CYCLE)
+        self.pwm.freq(freq_hz)
 
     def stop(self):
-        self.pwm_sig.duty_cycle(0)
+        self.pwm.duty(0)
