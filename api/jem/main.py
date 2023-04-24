ba8be9c5d3d4a83cfba3cdb72a316f44bf722207
jthompson-ceres <jthompson@ceresimaging.net>
2023-04-23 21:24:12 -0700

    Add pcf8574.py driver and use in main.py for rc car test

--- api/jem/main.py
+++ api/jem/main.py
@@ -3,11 +3,29 @@
 #kit_main.load_kit()]
 from jemwifi import *
 from jem_help import jem_help
+from drivers.peripherals import JemI2C, I2C, JEM_DEFAULT_I2C_BUS, JEM_DEFAULT_I2C_BAUDRATE, JEM_DEFAULT_I2C_PINS
 
 global wlan
 wlan = setup_wifi()
 
-from kits.kit import Kit
+from drivers.pcf8574 import *
 
-kit = Kit()
-kit.start()
+from machine import Pin, I2C
+
+# i2c = I2C(0, scl=Pin(22), sda=Pin(21))
+i2c = I2C(JEM_DEFAULT_I2C_BUS, I2C.MASTER, pins=JEM_DEFAULT_I2C_PINS, baudrate=JEM_DEFAULT_I2C_BAUDRATE)
+pcf8574 = PCF8574(i2c, addr=0x20)
+
+# test out different combinations of 0b10101010
+
+def forward():
+    pcf8574.outputs(0b10101010) # you need to change these
+
+def backward():
+    pcf8574.outputs(0b10101010)
+
+def left():
+    pcf8574.outputs(0b10101010)
+
+def right():
+    pcf8574.outputs(0b10101010)
