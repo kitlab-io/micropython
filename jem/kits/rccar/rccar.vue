0c7654c36b985627897771041ac4c9f4d48d6545
Brandon <Ambition1906@users.noreply.github.com>
2023-12-07 14:15:26 -0800

    Update rccar.vue
    added torque slider

--- jem/kits/rccar/rccar.vue
+++ jem/kits/rccar/rccar.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="RCcar" :style="{'cursor': 'pointer'}">
-  <p>SimpleBot</p>
+  <p>RC-CAR</p>
 
     <!---- Move Buttons ----->
     <v-btn depressed v-on:click="stopMotion"> StopMotion </v-btn>
@@ -8,7 +8,10 @@
     <v-btn depressed v-on:mousedown="backward" v-on:mouseup="stopMotion"> Backward </v-btn>
     <v-btn depressed v-on:mousedown="left" v-on:mouseup="stopMotion"> Turn Left </v-btn>
     <v-btn depressed v-on:mousedown="right" v-on:mouseup="stopMotion"> Turn Right </v-btn>
-
+	
+    <!-- Color Slider -->
+  	<input type="range" min="0" max="1023" v-model="speed" @input="setspeed"> 
+  
   </div>
 </template>
 
@@ -21,6 +24,8 @@ module.exports = {
   },
   data: () => ({
     kitReady: false,
+    speed: 0,
+    speedTimer: null
   }),
 
   mounted(){
@@ -30,6 +35,20 @@ module.exports = {
   },
 
   methods: {
+    setspeed() {
+       
+       // NOTE: make sure you include newline and carriage return \n\r in your cmd since using the REPL
+       let cmd = `kit._kit.car.set_speed(${this.speed})\n\r`; // create the micropython cmd to set buzzer
+       if(this.speed == 0){
+          cmd = `kit._kit.car.stop()\n\r`; // if freq 0 just stop buzzer
+       }
+
+       if(this.speedTimer){
+           clearTimeout(this.speedTimer); // if we already have a cmd pending, cancel it and use latest one
+       }
+       this.speedTimer = setTimeout(()=>{ this.parent.device.send(cmd) }, 100); // send raw MicroPython REPL cmd after 100 millisec
+    },
+    
     kitResponseHandler(resp){
       console.log("kit response " + resp);
     },
